fd00da197ff3ce92bd0f6df00bf9f05c986820e5
Pan4itos21 <47004948+Pan4itos21@users.noreply.github.com>
2019-06-13 02:34:55 +0300

    Update easy4.m

--- panina-elena/easy4.m
+++ panina-elena/easy4.m
@@ -14,20 +14,20 @@
 rinexe('SITE247J.01N','eph.dat');
 Eph = get_eph('eph.dat');
 %----
-sr_kv = 0; %для вычисления значения ср.кв.
+sr_kv = 0; %РґР»СЏ РІС‹С‡РёСЃР»РµРЅРёСЏ Р·РЅР°С‡РµРЅРёСЏ СЃСЂ.РєРІ.
 
-mean_dif_me = [];%Receiver - Запись ср.кв. для N кол-ва повторов на текущей сигме 
-mean_dif_me1 = [];%Baseline - Запись ср.кв. для N кол-ва повторов на текущей сигме
-zn_sigm = []; %для записи значений сигмы (для вывода на график)
+mean_dif_me = [];%Receiver - Р—Р°РїРёСЃСЊ СЃСЂ.РєРІ. РґР»СЏ N РєРѕР»-РІР° РїРѕРІС‚РѕСЂРѕРІ РЅР° С‚РµРєСѓС‰РµР№ СЃРёРіРјРµ 
+mean_dif_me1 = [];%Baseline - Р—Р°РїРёСЃСЊ СЃСЂ.РєРІ. РґР»СЏ N РєРѕР»-РІР° РїРѕРІС‚РѕСЂРѕРІ РЅР° С‚РµРєСѓС‰РµР№ СЃРёРіРјРµ
+zn_sigm = []; %РґР»СЏ Р·Р°РїРёСЃРё Р·РЅР°С‡РµРЅРёР№ СЃРёРіРјС‹ (РґР»СЏ РІС‹РІРѕРґР° РЅР° РіСЂР°С„РёРє)
     
 sigm = 5;
-for sigm = 5:1:8 % цикл по изменению сигмы(от 5 до 8)
+for sigm = 5:1:8 % С†РёРєР» РїРѕ РёР·РјРµРЅРµРЅРёСЋ СЃРёРіРјС‹(РѕС‚ 5 РґРѕ 8)
 
     mean_sigm = [];    
-    dif_me = [];%Receiver - записываем ВСЕ XYZ значения для каждой итерации повтора рассчета [3*gend]
-    dif_me1 = [];%Baseline - записываем ВСЕ XYZ значения для каждой итерации повтора рассчета [3*gend]
+    dif_me = [];%Receiver - Р·Р°РїРёСЃС‹РІР°РµРј Р’РЎР• XYZ Р·РЅР°С‡РµРЅРёСЏ РґР»СЏ РєР°Р¶РґРѕР№ РёС‚РµСЂР°С†РёРё РїРѕРІС‚РѕСЂР° СЂР°СЃСЃС‡РµС‚Р° [3*gend]
+    dif_me1 = [];%Baseline - Р·Р°РїРёСЃС‹РІР°РµРј Р’РЎР• XYZ Р·РЅР°С‡РµРЅРёСЏ РґР»СЏ РєР°Р¶РґРѕР№ РёС‚РµСЂР°С†РёРё РїРѕРІС‚РѕСЂР° СЂР°СЃСЃС‡РµС‚Р° [3*gend]
     
-    gend = 100; %цикл по количеству повторов рассчета
+    gend = 100; %С†РёРєР» РїРѕ РєРѕР»РёС‡РµСЃС‚РІСѓ РїРѕРІС‚РѕСЂРѕРІ СЂР°СЃСЃС‡РµС‚Р°
     for g = 1:gend
         % We identify the master observation file and open it
         ofile1 = 'SITE24~1.01O';
@@ -38,33 +38,33 @@ for sigm = 5:1:8 % цикл по изменению сигмы(от 5 до 8)
         rand_Pos =[];
         Gdop = [];
         
-        error = randn(7,1)*sigm; %ШУМ ДЛЯ ДОБАВЛЕНИЯ!!!
+        error = randn(7,1)*sigm; %РЁРЈРњ Р”Р›РЇ Р”РћР‘РђР’Р›Р•РќРРЇ!!!
                 
         % There are 22 epochs of data in ofile1
         qend = 22;
-        for q = 1:qend %по эпоам
+        for q = 1:qend %РїРѕ СЌРїРѕР°Рј
             [time1, dt1, sats1, eof1] = fepoch_0(fid1);
             NoSv1 = size(sats1,1);
             % We pick the observed C1 pseudoranges
             obs1 = grabdata(fid1, NoSv1, NoObs_types1);
             i = fobs_typ(Obs_types1,'C1');
             
-            %without error БЕЗ ШУМА
+            %without error Р‘Р•Р— РЁРЈРњРђ
             [pos, el, gdop] = recpo_ls(obs1(:,i),sats1,time1,Eph);%obs1(:,i)+randn(7,1)*10
             Gdop = [Gdop gdop];
             Pos = [Pos pos];
-            %with error  ДОБАВЛЯЕМ ШУМ
+            %with error  Р”РћР‘РђР’Р›РЇР•Рњ РЁРЈРњ
             rand_obs=obs1(:,i)+error;
             [rand_pos, el, gdop] = recpo_ls(rand_obs(:,i),sats1,time1,Eph);
             rand_Pos = [rand_Pos rand_pos];
         end
         
         %without error
-        me = mean(Pos,2); % Получаем координаты xyz        
+        me = mean(Pos,2); % РџРѕР»СѓС‡Р°РµРј РєРѕРѕСЂРґРёРЅР°С‚С‹ xyz        
         %with error
-        rand_me = mean(rand_Pos,2);% Получаем координаты xyz
+        rand_me = mean(rand_Pos,2);% РџРѕР»СѓС‡Р°РµРј РєРѕРѕСЂРґРёРЅР°С‚С‹ xyz
         
-        %записываем значения для каждой итерации повтора рассчета [3*gend]
+        %Р·Р°РїРёСЃС‹РІР°РµРј Р·РЅР°С‡РµРЅРёСЏ РґР»СЏ РєР°Р¶РґРѕР№ РёС‚РµСЂР°С†РёРё РїРѕРІС‚РѕСЂР° СЂР°СЃСЃС‡РµС‚Р° [3*gend]
         dif_me = [dif_me rand_me];        
         %------------        
         % we need to close all open files and then open to read from the beginning
@@ -107,41 +107,41 @@ for sigm = 5:1:8 % цикл по изменению сигмы(от 5 до 8)
             end
             %master observations: obs1, and rover observations: obs2
             
-            %without error БЕЗ ШУМА
+            %without error Р‘Р•Р— РЁРЈРњРђ
             [omc,base] = baseline(master_pos,obs1,obs2,sats1,time1,Eph);%obs1+randn(7,1)*10
             Omc = [Omc, omc];
             bases = [bases base];
-            %with error ДОБАВЛЯЕМ ШУМ
+            %with error Р”РћР‘РђР’Р›РЇР•Рњ РЁРЈРњ
             rand_obs1=obs1+error;
             [rand_omc,rand_base] = baseline(master_pos,rand_obs1,obs2,sats1,time1,Eph);
             rand_Omc = [rand_Omc, rand_omc];
             rand_bases = [rand_bases rand_base];
         end
         %without error
-        me1 = mean(bases,2);%Получаем координаты xyz        
+        me1 = mean(bases,2);%РџРѕР»СѓС‡Р°РµРј РєРѕРѕСЂРґРёРЅР°С‚С‹ xyz        
         %with error
-        rand_me1 = mean(rand_bases,2);%Получаем координаты xyz
+        rand_me1 = mean(rand_bases,2);%РџРѕР»СѓС‡Р°РµРј РєРѕРѕСЂРґРёРЅР°С‚С‹ xyz
         
-        %записываем значения для каждой итерации повтора рассчета [3*gend]
+        %Р·Р°РїРёСЃС‹РІР°РµРј Р·РЅР°С‡РµРЅРёСЏ РґР»СЏ РєР°Р¶РґРѕР№ РёС‚РµСЂР°С†РёРё РїРѕРІС‚РѕСЂР° СЂР°СЃСЃС‡РµС‚Р° [3*gend]
         dif_me1 = [dif_me1 rand_me1];
                 
     end
-        %Receiver - считаем среднее квадр. для N кол-ва повторов на текущей сигме
+        %Receiver - СЃС‡РёС‚Р°РµРј СЃСЂРµРґРЅРµРµ РєРІР°РґСЂ. РґР»СЏ N РєРѕР»-РІР° РїРѕРІС‚РѕСЂРѕРІ РЅР° С‚РµРєСѓС‰РµР№ СЃРёРіРјРµ
         sr_kv = std(dif_me,1,2);        
-        %Receiver - Запись ср.кв. для N кол-ва повторов на текущей сигме
+        %Receiver - Р—Р°РїРёСЃСЊ СЃСЂ.РєРІ. РґР»СЏ N РєРѕР»-РІР° РїРѕРІС‚РѕСЂРѕРІ РЅР° С‚РµРєСѓС‰РµР№ СЃРёРіРјРµ
         mean_dif_me = [mean_dif_me sr_kv];
     
-        %Baseline - считаем среднее квадр. для N кол-ва повторов на текущей сигме
+        %Baseline - СЃС‡РёС‚Р°РµРј СЃСЂРµРґРЅРµРµ РєРІР°РґСЂ. РґР»СЏ N РєРѕР»-РІР° РїРѕРІС‚РѕСЂРѕРІ РЅР° С‚РµРєСѓС‰РµР№ СЃРёРіРјРµ
         sr_kv1 = std(dif_me1,1,2);        
-        %Baseline - Запись ср.кв. для N кол-ва повторов на текущей сигме
+        %Baseline - Р—Р°РїРёСЃСЊ СЃСЂ.РєРІ. РґР»СЏ N РєРѕР»-РІР° РїРѕРІС‚РѕСЂРѕРІ РЅР° С‚РµРєСѓС‰РµР№ СЃРёРіРјРµ
         mean_dif_me1 = [mean_dif_me1 sr_kv1];    
         
-        %копим значения сигмы (чтобы график вывести)
+        %РєРѕРїРёРј Р·РЅР°С‡РµРЅРёСЏ СЃРёРіРјС‹ (С‡С‚РѕР±С‹ РіСЂР°С„РёРє РІС‹РІРµСЃС‚Рё)
         zn_sigm = [zn_sigm sigm];
 end
-    %------ГРАФИКИ
+    %------Р“Р РђР¤РРљР
      figure(1);
-     % по оси х значения сигмы, по у ср.кв.
+     % РїРѕ РѕСЃРё С… Р·РЅР°С‡РµРЅРёСЏ СЃРёРіРјС‹, РїРѕ Сѓ СЃСЂ.РєРІ.
      plot(zn_sigm, mean_dif_me(1:3,:),'linewidth',2)
      title('Variation of Receiver ','fontsize',16)
      legend('X','Y','Z')
@@ -150,7 +150,7 @@ end
      print -depsc easy4_1
      
      figure(2);
-     % по оси х значения сигмы, по у ср.кв.
+     % РїРѕ РѕСЃРё С… Р·РЅР°С‡РµРЅРёСЏ СЃРёРіРјС‹, РїРѕ Сѓ СЃСЂ.РєРІ.
      plot(zn_sigm, mean_dif_me1(1:3,:),'linewidth',2)
      title('Variation of Baseline Components ','fontsize',16)
      legend('X','Y','Z')
